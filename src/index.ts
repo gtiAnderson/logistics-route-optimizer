b7e36c215a475d3575d01209c1ef89cdaaa53d80
Anderson Moreira <andersonmoreira507@gmail.com>
2025-05-08 15:29:53 -0300

    feat: implementing calculation of Euclidean distance between two points

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ interface PontoEntrega {
     x: number;
     y: number;
 }
-// Entrada de dados: lista de pontos de entrega
+
+//função da formula euclidiana
+function calcularDistancia(a: PontoEntrega, b: PontoEntrega): number {
+    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+}
+
+//Entrada de dados: lista de pontos de entrega
 const pontos: PontoEntrega[] = [
     { id: 1, x: 0, y: 0 },
     { id: 2, x: 10, y: 5 },
@@ -13,4 +19,3 @@ const pontos: PontoEntrega[] = [
     { id: 4, x: 8, y: 3 },
     { id: 5, x: 2, y: 8 },
 ];
-
